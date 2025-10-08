4cf23267cf29c6764db6d1a2070b97f6fdd48903
Neotyx <Luuk.stam05@gmail.com>
2025-10-08 13:34:02 +0200

    UC14:
    Prijs toevoegen aan product class en uitbreiden constructor chain.
    ProductRepository --> prijsveld vullen met waarden.
    ProductView uitbreiden met kolom voor de prijs (header en inhoud van de tabel).

---
+++ Grocery.Core/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Grocery.Core.Models
+{
+    public class Category : Model
+    {
+        public Category(int id, string name) : base(id, name) { }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+    }
+}
